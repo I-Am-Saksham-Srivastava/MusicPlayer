d8509808a7ffe436860b08a68402508a98573768
Saksham Srivastava <84475573+I-Am-Saksham-Srivastava@users.noreply.github.com>
2022-03-12 22:44:52 +0530

    Create music-list.js

--- music-list.js
+++ music-list.js
@@ -13,38 +13,38 @@ let allMusic = [
   {
     name: "Harley Bird - Home",
     artist: "Jordan Schor",
-    img: "music-1",
-    src: "music-1"
+    img: "Assets/Poster/music-1.jpg",
+    src: "Assets/Music/music-1.mp3"
   },
   {
     name: "Ikson Anywhere – Ikson",
     artist: "Audio Library",
-    img: "music-2",
-    src: "music-2"
+    img: "Assets/Poster/music-2.jpg",
+    src: "Assets/Music/music-2.mp3"
   },
   {
     name: "Beauz & Jvna - Crazy",
     artist: "Beauz & Jvna",
-    img: "music-3",
-    src: "music-3"
+    img: "Assets/Poster/music-3.jpg",
+    src: "Assets/Music/music-3.mp3"
   },
   {
     name: "Hardwind - Want Me",
     artist: "Mike Archangelo",
-    img: "music-4",
-    src: "music-4"
+    img: "Assets/Poster/music-4.jpg",
+    src: "Assets/Music/music-4.mp3"
   },
   {
     name: "Jim - Sun Goes Down",
     artist: "Jim Yosef x Roy",
-    img: "music-5",
-    src: "music-5"
+    img: "Assets/Poster/music-5.jpg",
+    src: "Assets/Music/music-5.mp3"
   },
   {
     name: "Lost Sky - Vision NCS",
     artist: "NCS Release",
-    img: "music-6",
-    src: "music-6"
+    img: "Assets/Poster/music-6.jpg",
+    src: "Assets/Music/music-6.mp3"
   },
   // like this paste it and remember to give comma after ending of this bracket }
   // {
